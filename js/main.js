dedf920de71fbb1eee0054051012a03db07ecac9
maaax7 <maxtuka@gmail.com>
2023-03-07 14:08:05 +0000

    Capturando os dados da tela

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('novoItem')
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault(); 
+    
+    console.log(evento.target);
+    console.log(evento.target.elements['nome'].value);
+    console.log(evento.target.elements['quantidade'].value);
+});
