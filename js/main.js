ccdb1d5a94fc5122fe4c62b5139525bd0e0d1403
maaax7 <maxtuka@gmail.com>
2023-03-07 14:36:51 +0000

    Criando as validacoes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 const form = document.getElementById('novoItem')
-
+const lista = document.getElementById('lista');
+    
 form.addEventListener('submit', (evento) => {
     evento.preventDefault(); 
-    
-    console.log(evento.target);
-    console.log(evento.target.elements['nome'].value);
-    console.log(evento.target.elements['quantidade'].value);
+    let nome = evento.target.elements['nome'].value;
+    let quantidade = evento.target.elements['quantidade'].value;
+
+    criaElemento(nome, quantidade);
 });
+
+function criaElemento(nome, qtd) {
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('item');
+    
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = qtd;
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem);
+}
